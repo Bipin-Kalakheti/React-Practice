b429f8006add65d0900f09bc2a4741370ba62826
Bipin_Kalakheti <100770518+Bipin-Kalakheti@users.noreply.github.com>
2024-05-01 23:42:01 -0400

    implement stopwatch, using  usestate, useEffect, useRef

---
+++ Stopwatch/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+import Stopwatch from './Stopwatch.jsx'
+
+function App() {
+  
+
+  return (
+    <>
+      <Stopwatch />  
+    </>
+  )
+}
+
+export default App
